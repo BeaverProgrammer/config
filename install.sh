31a991b678c952ce92201bcfb7012045d7dbd74b
beaverprogrammer <cselftrain@icloud.com>
2017-03-25 21:24:21 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -11,13 +11,13 @@ sudo apt-get install git
 # install vim
 sudo apt-get install vim
 # install vim plugin
-# git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 # open vim , :PluginInstall
 
 # install nodejs
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -y nodejs
-sudo npm install -g npm@latest
+sudo npm install -g npm@latest jshint grunt-cli 
 
 # install tmux
 git clone https://github.com/tmux/tmux.git ~/github/tmux/
